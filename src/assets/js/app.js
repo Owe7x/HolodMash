46d6eb9453da440bfc42f43e5cab9e093e3905df
Timur <thedickst@gmail.com>
2021-06-15 15:55:29 +0300

    New Fix

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -157,3 +157,12 @@ $(document).ready(function() {
         $('body').toggleClass('fixed-page');
 	});
 });
+
+$('.buznes-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  arrows: false
+});
